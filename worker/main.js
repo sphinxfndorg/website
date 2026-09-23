d3e445a79ab01478e0b13b9bebc6ce808acb9268
pscs <119119194+ckusuma28@users.noreply.github.com>
2026-09-23 23:27:33 +0700

    updated

--- worker/main.js
+++ worker/main.js
@@ -8,6 +8,11 @@ export default {
       return env.ASSETS.fetch(wpUrl);
     }
 
+    if (url.hostname === "blocks.sphinxfnd.org") {
+      const blocksUrl = new URL("/blocks" + url.pathname, url);
+      return env.ASSETS.fetch(blocksUrl);
+    }
+
     if (url.hostname === "foundation.sphinxfnd.org") {
       const foundationUrl = new URL("/fnd" + url.pathname, url);
       return env.ASSETS.fetch(foundationUrl);
