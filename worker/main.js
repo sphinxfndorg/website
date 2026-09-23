f08dd3dcc47a9c325068694cea4d19283ca42e7e
pscs <119119194+ckusuma28@users.noreply.github.com>
2026-09-23 22:56:43 +0700

    add whitepaper

--- worker/main.js
+++ worker/main.js
@@ -3,6 +3,11 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
 
+    if (url.hostname === "whitepaper.sphinxfnd.org") {
+      const wpUrl = new URL("/wp" + url.pathname, url);
+      return env.ASSETS.fetch(wpUrl);
+    }
+
     if (url.hostname === "foundation.sphinxfnd.org") {
       const foundationUrl = new URL("/fnd" + url.pathname, url);
       return env.ASSETS.fetch(foundationUrl);
